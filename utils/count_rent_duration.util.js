44f721d54690a9864542457c9a72a68e8e535b97
beeerlian <naufal.berlian.99@gmail.com>
2022-12-20 02:33:05 +0700

    update calculate fee

--- utils/count_rent_duration.util.js
+++ utils/count_rent_duration.util.js
@@ -1,15 +1,14 @@
 
 
 
-module.exports = (date) => {
-       console.log(parseInt(date))
-       let rentDay = new Date(parseInt(date))
-       const currentDate = new Date();
-       console.log(`${rentDay} - ${currentDate}`);
 
+module.exports = (date) => {
+       let rentDay = new Date(date);
+       const currentDate = new Date();
 
        let totalRentHours = currentDate.getHours() - rentDay.getHours();
        let totalRentDay = currentDate.getDay() - rentDay.getDay();
+
        if (totalRentDay > 0) {
               totalRentHours = (24 * totalRentDay) + totalRentHours;
        }
@@ -18,4 +17,25 @@ module.exports = (date) => {
 
 
        return totalRent === 0 ? 1 : totalRent;
-}
+};
+
+
+
+// module.exports = (date) => {
+//        console.log(parseInt(date))
+//        let rentDay = new Date(parseInt(date))
+//        const currentDate = new Date();
+//        console.log(`${rentDay} - ${currentDate}`);
+
+
+//        let totalRentHours = currentDate.getHours() - rentDay.getHours();
+//        let totalRentDay = currentDate.getDay() - rentDay.getDay();
+//        if (totalRentDay > 0) {
+//               totalRentHours = (24 * totalRentDay) + totalRentHours;
+//        }
+
+//        const totalRent = Math.ceil(totalRentHours / 24);
+
+
+//        return totalRent === 0 ? 1 : totalRent;
+// }
